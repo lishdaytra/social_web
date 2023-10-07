e0f75f83f6c5f88eb811d528b6aa2d3c94ce9a25
lish_da_ytra <voleyballnikita@gmail.com>
2023-10-07 21:43:46 +0300

    Добавил группы при регистрации

--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from users.forms import CustomUserCreationForm
+from django.contrib.auth.models import Group
+from django.shortcuts import render, redirect
 
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+    # Set user Group on registration
+    def post(self, request, *args, **kwargs):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            # One user Group
+            user_group = Group.objects.get_by_natural_key(name=form.cleaned_data['groups'])
+            user.groups.add(user_group)
+            return redirect('login')
+        else:
+            return render(request, self.template_name, {'form' : form})
