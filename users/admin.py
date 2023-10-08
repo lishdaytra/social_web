5f1d3b862a6781e34102cbb44153ecc2e30acbb8
lish_da_ytra <voleyballnikita@gmail.com>
2023-10-08 23:15:39 +0300

    изменил поля first/last_name, добавил их для регистрации

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
+from users.models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
+    model = CustomUser
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active',)
-    list_filter = ('email', 'is_staff', 'is_active',)
+    list_display = ('email', 'is_staff', 'is_active', 'last_name')
+    list_filter = ('email', 'is_staff', 'is_active', 'last_name')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
--- users/forms.py
+++ users/forms.py
@@ -9,10 +9,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'gender', 'birth_date', 'groups')
+        fields = ('email', 'first_name', 'last_name', 'gender', 'birth_date', 'groups')
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'gender', 'birth_date', 'groups')
+        fields = ('email', 'first_name', 'last_name', 'gender', 'birth_date', 'groups')
